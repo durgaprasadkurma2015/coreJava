184ded468c1551a8eb70362c05a99060d783237c
Kurma Durga Prasad <durgaprasadkurma2015@gmail.com>
2026-08-08 08:48:33 +0530

    Add output

--- src/$1basic/basic.java
+++ src/$1basic/basic.java
@@ -2,6 +2,6 @@ package $1basic;
 
 public class basic {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); // output :- Hello World
 	}
 }
