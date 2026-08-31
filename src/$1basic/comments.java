98dc229877da9155b1e0d6b32c9c2d4fddbedf3c
Kurma Durga Prasad <durgaprasadkurma2015@gmail.com>
2026-08-31 10:34:02 +0530

    comments

---
+++ src/$1basic/comments.java
@@ -0,0 +1,13 @@
+package $1basic;
+
+
+public class comments {
+
+	public static void main(String[] args) {
+		
+System.out.println("// Single Comments");
+System.out.println("/** Multiple Comments/");
+
+	}
+
+}
